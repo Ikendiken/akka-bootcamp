f0c21651596125afff10ba5b52a1601192089416
Ikendiken <dave.allardyce@gmail.com>
2017-12-05 20:58:21 -0500

    Unit 2.3 Step 5

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -10,30 +10,45 @@ namespace WinTail
     class ConsoleReaderActor : UntypedActor
     {
         public const string ExitCommand = "exit";
-        private IActorRef _consoleWriterActor;
-
-        public ConsoleReaderActor(IActorRef consoleWriterActor)
+        public const string StartCommand = "start";
+        private readonly IActorRef _validationActor;
+        
+        public ConsoleReaderActor(IActorRef validationActor)
         {
-            _consoleWriterActor = consoleWriterActor;
+            _validationActor = validationActor;
         }
 
         protected override void OnReceive(object message)
         {
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            if (message.Equals(StartCommand))
             {
-                // shut down the system (acquire handle to system via
-                // this actors context)
+                DoPrintInstructions();
+            }
+
+            GetAndValidateInput();
+        }
+
+        #region Internal methods
+        private void DoPrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console!");
+            Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
+            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        }
+
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(message) == false && string.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            {
                 Context.System.Terminate();
                 return;
             }
 
-            // send input to the console writer to process and print
-            // YOU NEED TO FILL IN HERE
-
-            // continue reading messages from the console
-            // YOU NEED TO FILL IN HERE
+            _validationActor.Tell(message);
         }
+#endregion
 
     }
 }
---
+++ src/Unit-2/DoThis/Actors/PerformanceCounterCoordinatorActor.cs
@@ -0,0 +1,31 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartApp.Actors
+{
+    public class PerformanceCounterCoordinatorActor : ReceiveActor
+    {
+        public class Watch
+        {
+            public Watch(CounterType counter)
+            {
+                Counter = counter;
+            }
+
+            public CounterType Counter { get; private set; }
+        }
+
+        public class Unwatch
+        {
+            public Unwatch(CounterType counter)
+            {
+                Counter = counter;
+            }
+            public CounterType Counter { get; private set; }
+        }
+    }
+}
